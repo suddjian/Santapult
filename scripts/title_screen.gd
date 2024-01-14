066eadac2251a2b4a5475a8ea17afaa6e205b049
David Aaron <aasuddjian@gmail.com>
2024-01-13 16:17:08 -0800

    press anything indeed

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+func is_ignored_key(event: InputEvent):
+	if not (event is InputEventKey):
+		return false
+	return event.keycode > KEY_MENU and event.keycode < KEY_LAUNCHMEDIA
+
 func _input(event):
-	if event.is_released():
+	if (event is InputEventKey or event is InputEventJoypadButton or event is InputEventKey or event is InputEventMouseButton) and not is_ignored_key(event) and event.is_released():
 		get_tree().change_scene_to_file("res://scenes/Game.tscn")
